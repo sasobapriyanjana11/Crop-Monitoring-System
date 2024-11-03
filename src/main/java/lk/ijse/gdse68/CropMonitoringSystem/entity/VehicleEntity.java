6cded19a6f9630e7b2d2b3da679e62fa593a0846
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:32:52 +0530

    create VehicleEntity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/VehicleEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/VehicleEntity.java
@@ -1,9 +1,36 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//@Entity
+//@Table(name = "vehicle")
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class VehicleEntity implements SuperEntity {
+//
+//    @Id
+//    private String vehicleCode;
+//
+//    private String licensePlate;
+//    private String category;
+//    private String fuelType;
+//    private String status;
+//    private String remarks;
+//
+//    @OneToOne(mappedBy = "allocatedVehicle", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private StaffEntity assignedDriver;
+//}
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,12 +40,20 @@ import lombok.NoArgsConstructor;
 public class VehicleEntity implements SuperEntity {
     @Id
     private String vehicleCode;
-    private String licensePlate;
-    private String category;
+    private String licensePlateNumber;
+    private String vehicleCategory;
     private String fuelType;
     private String status;
-    private String remarks;
 
-    @OneToOne(mappedBy = "vehicle",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private StaffEntity assignedDriver;
+    @ManyToMany
+    @JoinTable(
+            name = "vehicle_staff",
+            joinColumns = @JoinColumn(name = "vehicleCode"),
+            inverseJoinColumns = @JoinColumn(name = "staffId")
+    )
+    private List<StaffEntity> staff;
+
+    @Column(columnDefinition = "TEXT")
+    private String remarks;
 }
+
