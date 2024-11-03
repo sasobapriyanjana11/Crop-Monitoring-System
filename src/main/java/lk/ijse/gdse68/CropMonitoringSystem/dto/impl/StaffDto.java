ec5de4633f2b7c4965c70da364ba035a64d823f8
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:40:36 +0530

    create Staff dto

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/StaffDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/StaffDto.java
@@ -1,24 +1,58 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
+//
+//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//
+//public class StaffDto implements SuperDto {
+//
+//    private String id;
+//    private String firstName;
+//    private String lastName;
+//    private String designation;
+//    private LocalDate joinDate;
+//    private LocalDate DOB;
+//    private String buildingNo;
+//    private String lane;
+//    private String city;
+//    private String state;
+//    private String postalCode;
+//    private String contactNo;
+//    private String email;
+//
+//    private List<String> fieldCodes; // List of FieldEntity's fieldCodes
+//    private String vehicleCode;
+//
+//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
+import lk.ijse.gdse68.CropMonitoringSystem.customObj.StaffResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
-public class StaffDto implements SuperDto {
+public class StaffDto implements SuperDto, StaffResponse {
 
     private String id;
     private String firstName;
     private String lastName;
     private String designation;
-    private LocalDate joinDate;
-    private LocalDate DOB;
+    private String gender;
+    private String joinedDate;
+    private String dob;
     private String buildingNo;
     private String lane;
     private String city;
@@ -26,8 +60,8 @@ public class StaffDto implements SuperDto {
     private String postalCode;
     private String contactNo;
     private String email;
-
-    private List<String> fieldCodes; // List of FieldEntity's fieldCodes
-    private String vehicleCode;
-
+    private String role;
+    private List<String> fieldCodes; // List of field codes assigned to this staff member
+    private List<String> vehicleCodes; // List of vehicle codes assigned to this staff member
 }
+
