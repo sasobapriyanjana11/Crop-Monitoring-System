f71f2d0a26acd3a51c6f7313693ea8b6bd9936da
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:28:36 +0530

    create user entity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/UserEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/UserEntity.java
@@ -1,9 +1,29 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//@Table(name = "user")
+//@Entity
+//public class UserEntity implements SuperEntity {
+//    @Id
+//    @Column(unique = true)
+//    private String email;
+//    private String password;
+//    private String role;
+//}
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lk.ijse.gdse68.CropMonitoringSystem.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +35,9 @@ import lombok.NoArgsConstructor;
 @Entity
 public class UserEntity implements SuperEntity {
     @Id
-    @Column(unique = true)
     private String email;
     private String password;
-    private String role;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
