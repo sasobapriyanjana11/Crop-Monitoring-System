1a0173c94d6e560091929bc29ad43912e27e0d16
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:39:03 +0530

    create crop dto

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/CropDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/CropDto.java
@@ -1,18 +1,39 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
+//
+//import lk.ijse.gdse68.CropMonitoringSystem.customObj.CropResponse;
+//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import java.util.List;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class CropDto implements SuperDto, CropResponse {
+//
+//    private String cropCode;
+//    private String commonName;
+//    private String scientificName;
+//    private String image;
+//    private String category;
+//    private String cropSeason;
+//    private String fieldCode;
+//    private List<CropDetailsDto>cropDetailsEntities;
+//
+//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
-import jakarta.persistence.*;
+import lk.ijse.gdse68.CropMonitoringSystem.customObj.CropResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
-import lk.ijse.gdse68.CropMonitoringSystem.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CropDto implements SuperDto {
+public class CropDto implements SuperDto, CropResponse {
 
     private String cropCode;
     private String commonName;
@@ -20,7 +41,6 @@ public class CropDto implements SuperDto {
     private String image;
     private String category;
     private String cropSeason;
-    private String fieldCode;
-    private List<CropDetailsDto>cropDetailsEntities;
-
+    private String fieldCode; // Field code associated with this crop
 }
+
