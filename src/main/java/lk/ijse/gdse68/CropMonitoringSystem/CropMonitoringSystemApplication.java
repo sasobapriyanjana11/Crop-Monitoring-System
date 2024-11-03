2eb3b6a1ff78414d6e9c50fa87d769b365f29676
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:34:06 +0530

    create bean modelMapper

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/CropMonitoringSystemApplication.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/CropMonitoringSystemApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse68.CropMonitoringSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CropMonitoringSystemApplication {
@@ -9,5 +11,8 @@ public class CropMonitoringSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CropMonitoringSystemApplication.class, args);
 	}
-
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
