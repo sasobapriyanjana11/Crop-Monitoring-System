e68c990550ce69c3c2515a71b94cf4c4edb5fd75
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:43:13 +0530

    create AppUtil class and implementation

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/util/AppUtil.java
@@ -0,0 +1,41 @@
+package lk.ijse.gdse68.CropMonitoringSystem.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Base64;
+import java.util.UUID;
+
+public class AppUtil {
+    public static String createCropId(){
+        return "Crop- "+ UUID.randomUUID();
+    }
+
+    public static String createEquipmentId(){
+        return "Equipment- "+ UUID.randomUUID();
+    }
+
+    public static String createFieldId(){
+        return "Field- "+ UUID.randomUUID();
+    }
+
+    public static String createMonitoringLogId(){
+        return "MLog- "+ UUID.randomUUID();
+    }
+
+    public static String createStaffId(){
+        return "Staff- "+ UUID.randomUUID();
+    }
+
+
+    public static String toBase64ProfilePic(MultipartFile profilePic){
+        String proPicBase64=null;
+        try{
+            byte[] imageByteCollection= profilePic.getBytes();
+            proPicBase64= Base64.getEncoder().encodeToString(imageByteCollection);
+
+        }catch(Exception e ){
+            e.printStackTrace();
+        }
+        return proPicBase64;
+    }
+}
