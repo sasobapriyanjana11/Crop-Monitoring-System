d084a6a8458b491a9651f2d8e12339bcfc0754f9
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:40:53 +0530

    create user dto

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/UserDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/UserDto.java
@@ -1,14 +1,31 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
+//
+//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class UserDto implements SuperDto {
+//    private String email;
+//    private String password;
+//    private String role;
+//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto implements SuperDto {
+
     private String email;
     private String password;
     private String role;
 }
+
