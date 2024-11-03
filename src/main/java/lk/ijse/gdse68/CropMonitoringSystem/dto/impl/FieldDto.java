a4322db60a52c61505cecf8903a1fb9ba2ff004e
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:39:54 +0530

    create Field dto

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/FieldDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/FieldDto.java
@@ -1,22 +1,48 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
+//
+//import jakarta.persistence.*;
+//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
+//import lk.ijse.gdse68.CropMonitoringSystem.entity.SuperEntity;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.List;
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class FieldDto implements SuperDto {
+//
+//    private String fieldCode;
+//    private String fieldName;
+//    private Double extentSize;
+//    private String fieldLocation;
+//    private List<String> cropCodes;
+//    private List<String> staffIds;
+//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
-import jakarta.persistence.*;
+import lk.ijse.gdse68.CropMonitoringSystem.customObj.FieldResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
-import lk.ijse.gdse68.CropMonitoringSystem.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class FieldDto implements SuperDto {
+public class FieldDto implements SuperDto, FieldResponse {
 
     private String fieldCode;
     private String fieldName;
-    private Double extentSize;
-    private String fieldLocation;
-    private List<String> cropCodes;
-    private List<String> staffIds;
+    private String location; // GPS coordinates as String
+    private double extentSize;
+    private List<String> cropCodes; // List of crop codes related to the field
+    private List<String> staffIds; // List of staff IDs allocated to the field
+    private String fieldImage1;
+    private String fieldImage2;
 }
+
