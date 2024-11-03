f9f55f793586f1a1f95dacce1ee413a09510df5c
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:44:29 +0530

    create CropNotFoundException customObj class

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/exception/CropNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.CropMonitoringSystem.exception;
+
+public class CropNotFoundException extends RuntimeException{
+    public CropNotFoundException(){}
+    public CropNotFoundException(String msg){}
+    public CropNotFoundException(String msg,Throwable cause){}
+}
