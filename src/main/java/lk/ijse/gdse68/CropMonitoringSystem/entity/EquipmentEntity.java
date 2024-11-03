38920a29bf233766f33702a0e710e8ba6ad0e762
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:30:11 +0530

    create equipment entity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/EquipmentEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/EquipmentEntity.java
@@ -1,10 +1,37 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//@Table(name = "equipment")
+//@Entity
+//public class EquipmentEntity implements SuperEntity {
+//    @Id
+//    private String id;
+//    private String name;
+//    private String type;
+//    private String status;
+//    private String fieldCode;
+//    private String staffId;
+//
+//
+//}
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
 import jakarta.persistence.*;
+import lk.ijse.gdse68.CropMonitoringSystem.enums.EquipmentType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,12 +40,44 @@ import lombok.NoArgsConstructor;
 @Entity
 public class EquipmentEntity implements SuperEntity {
     @Id
-    private String id;
+    @Column(name = "equipment_id", nullable = false, updatable = false)
+    private String equipmentId;
     private String name;
-    private String type;
+
+    @Enumerated(EnumType.STRING)
+    private EquipmentType type;
+
     private String status;
-    private String fieldCode;
-    private String staffId;
 
+    @ManyToMany
+    @JoinTable(
+            name = "equipment_staff",
+            joinColumns = @JoinColumn(name = "equipment_id"),
+            inverseJoinColumns = @JoinColumn(name = "staffId")
+    )
+    private List<StaffEntity> staff;
 
+    @ManyToMany
+    @JoinTable(
+            name = "equipment_field",
+            joinColumns = @JoinColumn(name = "equipment_id"),
+            inverseJoinColumns = @JoinColumn(name = "fieldCode")
+    )
+    private List<FieldEntity> fields;
+
+    public void addFields(FieldEntity field) {
+        if (this.fields == null) {
+            this.fields = new ArrayList<>();
+        }
+        this.fields.add(field);
+    }
+
+    public void addStaff(StaffEntity staff) {
+        if (this.staff == null) {
+            this.staff = new ArrayList<>();
+        }
+        this.staff.add(staff);
+
+    }
 }
+
