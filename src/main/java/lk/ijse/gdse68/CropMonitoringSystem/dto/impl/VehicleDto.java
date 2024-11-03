ebeba5524446707a7ae8899c3d6b7c1738ec1f19
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-03 11:41:09 +0530

    create vehicle dto

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/VehicleDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/VehicleDto.java
@@ -1,9 +1,31 @@
+//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
+//
+//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class VehicleDto implements SuperDto {
+//
+//    private String vehicleCode;
+//    private String licensePlate;
+//    private String category;
+//    private String fuelType;
+//    private String status;
+//    private String remarks;
+//    private String staffId;
+//
+// }
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +37,6 @@ public class VehicleDto implements SuperDto {
     private String fuelType;
     private String status;
     private String remarks;
-    private String staffId;
+    private String staffId; // Assigned driver (staff member)
 }
+
