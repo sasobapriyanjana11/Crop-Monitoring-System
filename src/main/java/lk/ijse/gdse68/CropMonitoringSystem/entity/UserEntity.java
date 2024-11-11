4c8b99648b67e3343f0f3e277f479aa2d21c00fd
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 22:56:59 +0530

    update User entity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/UserEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/UserEntity.java
@@ -1,25 +1,3 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.entity;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Table(name = "user")
-//@Entity
-//public class UserEntity implements SuperEntity {
-//    @Id
-//    @Column(unique = true)
-//    private String email;
-//    private String password;
-//    private String role;
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
 import jakarta.persistence.*;
