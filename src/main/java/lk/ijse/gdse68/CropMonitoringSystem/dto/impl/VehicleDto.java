80843162d7f50cdfa275d66c5bcff6537593579b
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:05:16 +0530

    remove staffId

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/VehicleDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/VehicleDto.java
@@ -1,26 +1,6 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
-//
-//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class VehicleDto implements SuperDto {
-//
-//    private String vehicleCode;
-//    private String licensePlate;
-//    private String category;
-//    private String fuelType;
-//    private String status;
-//    private String remarks;
-//    private String staffId;
-//
-// }
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
+import lk.ijse.gdse68.CropMonitoringSystem.customObj.VehicleResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class VehicleDto implements SuperDto {
+public class VehicleDto implements SuperDto, VehicleResponse {
 
     private String vehicleCode;
     private String licensePlate;
@@ -37,6 +17,5 @@ public class VehicleDto implements SuperDto {
     private String fuelType;
     private String status;
     private String remarks;
-    private String staffId; // Assigned driver (staff member)
 }
 
