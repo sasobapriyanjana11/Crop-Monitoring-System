d904e3dd680b1f7735da9f3e87e6a97015c81dbe
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:07:48 +0530

    create MonitoringLog Error handling classes(customObj)

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/exception/MonitoringLogException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.CropMonitoringSystem.exception;
+
+public class MonitoringLogException extends RuntimeException {
+    public MonitoringLogException(){}
+    public MonitoringLogException(String message) {}
+    public MonitoringLogException(String message, Throwable cause) {}
+}
