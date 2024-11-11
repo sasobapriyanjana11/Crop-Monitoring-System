ca302999c48741e70de29a93b35bfadc72ebaa3a
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:13:14 +0530

    create MonitoringLogDao

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dao/MonitoringLogDao.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.CropMonitoringSystem.dao;
+
+import lk.ijse.gdse68.CropMonitoringSystem.entity.MonitoringLogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MonitoringLogDao extends JpaRepository<MonitoringLogEntity,String> {
+}
