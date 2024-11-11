dd05e96520b66dee0924a03971a57e1aded8ce28
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:03:59 +0530

    update DOB data type,add role

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/StaffDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/StaffDto.java
@@ -1,58 +1,27 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
-//
-//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//
-//public class StaffDto implements SuperDto {
-//
-//    private String id;
-//    private String firstName;
-//    private String lastName;
-//    private String designation;
-//    private LocalDate joinDate;
-//    private LocalDate DOB;
-//    private String buildingNo;
-//    private String lane;
-//    private String city;
-//    private String state;
-//    private String postalCode;
-//    private String contactNo;
-//    private String email;
-//
-//    private List<String> fieldCodes; // List of FieldEntity's fieldCodes
-//    private String vehicleCode;
-//
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lk.ijse.gdse68.CropMonitoringSystem.customObj.StaffResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class StaffDto implements SuperDto, StaffResponse {
 
-    private String id;
+    private String staffId;
     private String firstName;
     private String lastName;
     private String designation;
     private String gender;
-    private String joinedDate;
-    private String dob;
+    private LocalDate joinDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate DOB;
     private String buildingNo;
     private String lane;
     private String city;
@@ -60,8 +29,7 @@ public class StaffDto implements SuperDto, StaffResponse {
     private String postalCode;
     private String contactNo;
     private String email;
-    private String role;
-    private List<String> fieldCodes; // List of field codes assigned to this staff member
-    private List<String> vehicleCodes; // List of vehicle codes assigned to this staff member
+    private String equipmentCode;
+    private String vehicleCode;
 }
 
