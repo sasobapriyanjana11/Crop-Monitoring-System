6d96cedfad15e3a51cfb6c88e58fc6e00a1a999d
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:07:20 +0530

    create Field Error handling classes(customObj)

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/exception/FieldNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.CropMonitoringSystem.exception;
+
+public class FieldNotFoundException extends RuntimeException {
+    public FieldNotFoundException(){}
+    public FieldNotFoundException(String message) {}
+    public FieldNotFoundException(String message, Throwable cause) {}
+}
