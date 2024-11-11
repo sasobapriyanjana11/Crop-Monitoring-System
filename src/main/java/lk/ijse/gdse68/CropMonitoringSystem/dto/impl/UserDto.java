ca0602c59dc5c4c16302f7b05cc2d52802eb6e18
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:04:39 +0530

    update role data type String to Role

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/UserDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/UserDto.java
@@ -1,20 +1,7 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
-//
-//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class UserDto implements SuperDto {
-//    private String email;
-//    private String password;
-//    private String role;
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
+import lk.ijse.gdse68.CropMonitoringSystem.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,6 +13,6 @@ public class UserDto implements SuperDto {
 
     private String email;
     private String password;
-    private String role;
+    private Role role;
 }
 
