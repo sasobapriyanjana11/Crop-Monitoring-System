59f0d9f851b837a60933b5e15be9537b8daa3c39
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:00:28 +0530

    remove commented information

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/CropDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/CropDto.java
@@ -1,27 +1,3 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
-//
-//import lk.ijse.gdse68.CropMonitoringSystem.customObj.CropResponse;
-//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import java.util.List;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class CropDto implements SuperDto, CropResponse {
-//
-//    private String cropCode;
-//    private String commonName;
-//    private String scientificName;
-//    private String image;
-//    private String category;
-//    private String cropSeason;
-//    private String fieldCode;
-//    private List<CropDetailsDto>cropDetailsEntities;
-//
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
 import lk.ijse.gdse68.CropMonitoringSystem.customObj.CropResponse;
@@ -41,6 +17,6 @@ public class CropDto implements SuperDto, CropResponse {
     private String image;
     private String category;
     private String cropSeason;
-    private String fieldCode; // Field code associated with this crop
+    private String fieldCode;
 }
 
