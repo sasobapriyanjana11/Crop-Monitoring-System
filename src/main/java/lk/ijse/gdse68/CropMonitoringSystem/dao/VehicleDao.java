2900331683e5730040e990ba17937836a23cc121
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:13:56 +0530

    create VehicleDao

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dao/VehicleDao.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse68.CropMonitoringSystem.dao;
+
+import lk.ijse.gdse68.CropMonitoringSystem.entity.VehicleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleDao extends JpaRepository<VehicleEntity,String> {
+    VehicleEntity findByVehicleCode(String vehicleCode);
+}
