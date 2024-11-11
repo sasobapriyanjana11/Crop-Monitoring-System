97ce6b323c24b1ef5994340aca21d1b34c3a4770
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 22:57:32 +0530

    update Staff entity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/StaffEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/StaffEntity.java
@@ -1,74 +1,27 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import java.time.LocalDate;
-//import java.util.List;
-//@Entity
-//@Table(name = "staff")
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class StaffEntity implements SuperEntity {
-//
-//    @Id
-//    private String id;
-//
-//    private String firstName;
-//    private String lastName;
-//    private String designation;
-//    private LocalDate joinDate;
-//    private LocalDate DOB;
-//    private String buildingNo;
-//    private String lane;
-//    private String city;
-//    private String state;
-//    private String postalCode;
-//    private String contactNo;
-//    private String email;
-//
-//    @ManyToMany(mappedBy = "allocatedStaff", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-//    private List<FieldEntity> fields;
-//
-//    @OneToOne
-//    @JoinColumn(name = "vehicleCode")
-//    private VehicleEntity allocatedVehicle;
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
 import jakarta.persistence.*;
-import lk.ijse.gdse68.CropMonitoringSystem.customObj.StaffResponse;
-import lk.ijse.gdse68.CropMonitoringSystem.enums.Gender;
-import lk.ijse.gdse68.CropMonitoringSystem.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.Date;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "staff")
 @Entity
-public class StaffEntity implements StaffResponse,SuperEntity {
+@Table(name = "Staff")
+public class StaffEntity implements SuperEntity{
     @Id
-    private String id;
+    private String staffId;
     private String firstName;
     private String lastName;
     private String designation;
-
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-
-    @Temporal(TemporalType.DATE)
-    private Date joinedDate;
-
-    @Temporal(TemporalType.DATE)
-    private Date dob;
-
+    private String gender;
+    private LocalDate joinDate;
+    private LocalDate DOB;
     private String buildingNo;
     private String lane;
     private String city;
@@ -77,18 +30,21 @@ public class StaffEntity implements StaffResponse,SuperEntity {
     private String contactNo;
     private String email;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+    @ManyToMany
+    @JoinTable(name = "staff_log",
+            joinColumns = @JoinColumn(name = "staffId"),inverseJoinColumns = @JoinColumn(name = "logCode"))
+    private List<MonitoringLogEntity> staffLogEntities;
 
-    @ManyToMany(mappedBy = "staff")
-    private List<FieldEntity> fields;
+    @ManyToMany(mappedBy = "assignedStaff")
+    private List<FieldEntity> fieldEntities;
 
-    @ManyToMany(mappedBy = "staff")
-    private List<VehicleEntity> vehicles;
+    @ManyToOne
+    @JoinColumn(name ="equipmentCode")
+    private EquipmentEntity equipment;
 
-    @OneToMany(mappedBy = "staff", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<MonitoringLogEntity> monitoringLogs;
+    @ManyToOne
+    @JoinColumn(name = "vehicleCode")
+    private VehicleEntity vehicle;
 
-    @ManyToMany(mappedBy = "staff")
-    private List<EquipmentEntity> equipment;
+
 }
