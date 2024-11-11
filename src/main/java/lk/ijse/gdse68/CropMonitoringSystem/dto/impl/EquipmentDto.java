3acb2d2f998bb0a0947c6b18d1fc1414480980d2
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:01:02 +0530

    update EquipmentDto

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/EquipmentDto.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dto/impl/EquipmentDto.java
@@ -1,43 +1,21 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
-//
-//import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class EquipmentDto implements SuperDto {
-//
-//    private String id;
-//    private String name;
-//    private String type;
-//    private String status;
-//    private String fieldCode;
-//    private String staffId;
-//
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.dto.impl;
 
 import lk.ijse.gdse68.CropMonitoringSystem.customObj.EquipmentResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.SuperDto;
+import lk.ijse.gdse68.CropMonitoringSystem.enums.EquipmentType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EquipmentDto implements SuperDto, EquipmentResponse {
 
-    private String equipmentId;
-    private String name;
-    private String type;
+   private String equipmentCode;
+    private String equipmentName;
     private String status;
-    private List<String> staffIds; // List of assigned staff IDs
-    private List<String> fieldCodes;
+    private EquipmentType type;
 }
 
