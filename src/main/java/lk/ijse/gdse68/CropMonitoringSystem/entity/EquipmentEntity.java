cde7fd474a1d3da657ecb20fb61655e53c41b3b4
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 22:58:50 +0530

    update Equipment entity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/EquipmentEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/EquipmentEntity.java
@@ -1,27 +1,3 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Table(name = "equipment")
-//@Entity
-//public class EquipmentEntity implements SuperEntity {
-//    @Id
-//    private String id;
-//    private String name;
-//    private String type;
-//    private String status;
-//    private String fieldCode;
-//    private String staffId;
-//
-//
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
 import jakarta.persistence.*;
@@ -29,55 +5,25 @@ import lk.ijse.gdse68.CropMonitoringSystem.enums.EquipmentType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
-@Table(name = "equipment")
 @Entity
-public class EquipmentEntity implements SuperEntity {
+@Table(name = "Equipment")
+public class EquipmentEntity implements SuperEntity{
     @Id
-    @Column(name = "equipment_id", nullable = false, updatable = false)
-    private String equipmentId;
-    private String name;
+    private String equipmentCode;
 
+    private String equipmentName;
+    private String status;
     @Enumerated(EnumType.STRING)
     private EquipmentType type;
 
-    private String status;
+    @OneToMany(mappedBy = "equipment", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<StaffEntity> assignedStaff;
 
-    @ManyToMany
-    @JoinTable(
-            name = "equipment_staff",
-            joinColumns = @JoinColumn(name = "equipment_id"),
-            inverseJoinColumns = @JoinColumn(name = "staffId")
-    )
-    private List<StaffEntity> staff;
-
-    @ManyToMany
-    @JoinTable(
-            name = "equipment_field",
-            joinColumns = @JoinColumn(name = "equipment_id"),
-            inverseJoinColumns = @JoinColumn(name = "fieldCode")
-    )
-    private List<FieldEntity> fields;
-
-    public void addFields(FieldEntity field) {
-        if (this.fields == null) {
-            this.fields = new ArrayList<>();
-        }
-        this.fields.add(field);
-    }
-
-    public void addStaff(StaffEntity staff) {
-        if (this.staff == null) {
-            this.staff = new ArrayList<>();
-        }
-        this.staff.add(staff);
-
-    }
+    @OneToMany(mappedBy = "equipment",cascade = CascadeType.ALL,fetch = FetchType.LAZY )
+    private List<FieldEntity> fieldEntities ;
 }
-
