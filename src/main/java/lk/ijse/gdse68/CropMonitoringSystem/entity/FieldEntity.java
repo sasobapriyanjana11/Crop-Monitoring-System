874500f66c45ae4f13430442db4c3e0bf2118fd1
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 22:58:27 +0530

    update Field entity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/FieldEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/FieldEntity.java
@@ -1,32 +1,4 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import java.util.List;
-//@Entity
-//@Table(name = "field")
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class FieldEntity implements SuperEntity {
-//
-//    @Id
-//    private String fieldCode;
-//
-//    private String fieldName;
-//    private String location;
-//
-//    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-//    @JoinTable(
-//            name = "staff_field",
-//            joinColumns = @JoinColumn(name = "field_code"),
-//            inverseJoinColumns = @JoinColumn(name = "staff_id")
-//    )
-//    private List<StaffEntity> allocatedStaff;
-//}
+
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
 import jakarta.persistence.*;
@@ -39,45 +11,39 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "field")
 @Entity
-public class FieldEntity implements SuperEntity {
+@Table(name = "Field")
+public class FieldEntity implements SuperEntity{
     @Id
     private String fieldCode;
     private String fieldName;
-
-    @Column(columnDefinition = "POINT")
-    private String fieldLocation;  // Assuming GPS point is stored as String
-
     private Double extentSize;
 
-    @OneToMany(mappedBy = "field", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Column(columnDefinition = "POINT")
+    private String fieldLocation;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String image1;  // Renamed from "fieldImage1"
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String image2;  // Renamed from "fieldImage2"
+
+    @OneToMany(mappedBy = "field", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     private List<CropEntity> crops;
+
+    @ManyToOne
+    @JoinColumn(name = "equipmentCode")
+    private EquipmentEntity equipment;
 
     @ManyToMany
     @JoinTable(
-            name = "field_staff",
+            name = "field_staff_details",
             joinColumns = @JoinColumn(name = "fieldCode"),
             inverseJoinColumns = @JoinColumn(name = "staffId")
     )
-    private List<StaffEntity> staff;
+    private List<StaffEntity> assignedStaff;
 
-    @OneToMany(mappedBy = "field", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<MonitoringLogEntity> monitoringLogs;
-
-    @ManyToMany
-    @JoinTable(
-            name = "field_equipment",
-            joinColumns = @JoinColumn(name = "fieldCode"),
-            inverseJoinColumns = @JoinColumn(name = "equipment_id")
-    )
-    private List<EquipmentEntity> equipment;
-
-    @Column(columnDefinition = "LONGTEXT")
-    private String fieldImage1;
-
-    @Column(columnDefinition = "LONGTEXT")
-    private String fieldImage2;
-
+ @OneToMany(mappedBy ="field",cascade = CascadeType.ALL,fetch = FetchType.LAZY )
+    private List<MonitoringLogEntity>monitoringLog;
 
 }
