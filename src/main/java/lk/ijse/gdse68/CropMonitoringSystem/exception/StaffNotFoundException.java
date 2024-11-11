c7f8ed9e433c1b1fd654172cb4570ba03aba1995
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:08:17 +0530

    create Staff Error handling classes(customObj)

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/exception/StaffNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.CropMonitoringSystem.exception;
+
+public class StaffNotFoundException extends RuntimeException {
+    public StaffNotFoundException() {}
+    public StaffNotFoundException(String message) {}
+    public StaffNotFoundException(String message, Throwable cause) {}
+}
