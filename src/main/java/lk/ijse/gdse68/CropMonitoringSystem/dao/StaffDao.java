9109f89cf50b04bb7eb2f74c511fe1f65e161b74
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:13:33 +0530

    create StaffDao

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/dao/StaffDao.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.CropMonitoringSystem.dao;
+
+import lk.ijse.gdse68.CropMonitoringSystem.entity.StaffEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffDao extends JpaRepository<StaffEntity,String> {
+}
