953f859ee5141be8d1d5f22abe791b4b56f0df24
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 23:08:43 +0530

    create Vehicle Error handling classes(customObj)

---
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/exception/VehicleNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse68.CropMonitoringSystem.exception;
+
+public class VehicleNotFoundException extends RuntimeException {
+  public VehicleNotFoundException(){}
+    public VehicleNotFoundException(String message) {
+
+    }
+  public VehicleNotFoundException(String message, Throwable cause) {}
+}
