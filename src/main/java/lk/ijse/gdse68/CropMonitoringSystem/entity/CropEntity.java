8662308600b1af537000392053cda46a779427f2
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-11 22:56:18 +0530

    update Crop entity

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/CropEntity.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/entity/CropEntity.java
@@ -1,35 +1,3 @@
-//package lk.ijse.gdse68.CropMonitoringSystem.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import java.util.List;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Table(name = "crop")
-//@Entity
-//public class CropEntity implements SuperEntity {
-//    @Id
-//    private String cropCode;
-//    private String commonName;
-//    private String scientificName;
-//
-//    @Column(columnDefinition = "LONGTEXT")
-//    private String image;
-//    private String category;
-//    private String cropSeason;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "fieldCode")
-//    private FieldEntity field;
-//
-//    @OneToMany(mappedBy = "crop",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-//    private List<CropDetailsEntity> monitoringLogs;
-//}
 package lk.ijse.gdse68.CropMonitoringSystem.entity;
 
 import jakarta.persistence.*;
@@ -42,9 +10,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "crop")
 @Entity
-public class CropEntity implements SuperEntity {
+@Table(name = "Crop")
+public class CropEntity implements SuperEntity{
     @Id
     private String cropCode;
     private String commonName;
@@ -59,7 +27,9 @@ public class CropEntity implements SuperEntity {
     @JoinColumn(name = "fieldCode")
     private FieldEntity field;
 
-    @OneToMany(mappedBy = "crop", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<MonitoringLogEntity> monitoringLogs;
+    @ManyToMany
+    @JoinTable(name="crop_log",
+            joinColumns = @JoinColumn(name = "cropCode"),
+            inverseJoinColumns = @JoinColumn(name = "logCode"))
+    private List<MonitoringLogEntity> logEntities;
 }
-
