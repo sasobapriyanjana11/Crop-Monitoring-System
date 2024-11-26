60bb109cbd0e2afecbb985368c05af13c3d83eb8
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-11-26 13:09:21 +0530

    implement getCropById method and implement patch method to update associate entity crop_log

--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/controllor/CropController.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/controllor/CropController.java
@@ -1,11 +1,14 @@
 package lk.ijse.gdse68.CropMonitoringSystem.controllor;
 
+import lk.ijse.gdse68.CropMonitoringSystem.customObj.CropResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.impl.CropDto;
 import lk.ijse.gdse68.CropMonitoringSystem.exception.CropNotFoundException;
 import lk.ijse.gdse68.CropMonitoringSystem.exception.DataPersistFailedException;
 import lk.ijse.gdse68.CropMonitoringSystem.service.CropService;
 import lk.ijse.gdse68.CropMonitoringSystem.util.AppUtil;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,6 +24,8 @@ import java.util.List;
 public class CropController {
     @Autowired
     private CropService cropService;
+    static Logger logger = LoggerFactory.getLogger(CropController.class);
+
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void > createUser(
             @RequestPart("commonName")String commonName,
@@ -32,7 +37,7 @@ public class CropController {
          ){
 
         try {
-            String base64ProfilePic = AppUtil.toBase64ProfilePic(image);
+            String base64ProfilePic = AppUtil.toBase64Image(image);
             // build the user object
             CropDto buildCropDTO = new CropDto();
             buildCropDTO.setCommonName(commonName);
@@ -42,47 +47,48 @@ public class CropController {
             buildCropDTO.setCropSeason(cropSeason);
             buildCropDTO.setFieldCode(fieldCode);
 
-
             //send to the service layer
             cropService.saveCrop(buildCropDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistFailedException e){
+            logger.error("There was a error while updating");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @PatchMapping(value = "/{cropCode}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Void>  updateUser(@PathVariable("cropCode") String cropCode,
-                                            @RequestPart("commonName")String commonName,
-                                            @RequestPart("scientificName")String scientificName,
-                                            @RequestPart("image") MultipartFile image,
-                                            @RequestPart("category")String category,
-                                            @RequestPart("cropSeason")String cropSeason,
-                                            @RequestPart("fieldCode")String fieldCode
-                                           ){
+    @PatchMapping (value = "/{id}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity <Void> updateCrops (@PathVariable ("id") String id ,
+                                              @RequestParam("commonName") String commonName,
+                                              @RequestParam ("scientificName") String scientificName,
+                                              @RequestParam("image") MultipartFile image,
+                                              @RequestParam("category") String category,
+                                              @RequestParam("cropSeason") String cropSeason,
+                                              @RequestParam("fieldCode")String fieldCode,
+                                              @RequestParam("logCode")List<String> logCode
+    ){
+        //return userSERVICE.updateUser(id,userDTO) ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
         try {
 
-            String base64ProfilePic = AppUtil.toBase64ProfilePic(image);
-            CropDto updateCropDTO = new CropDto();
-            updateCropDTO.setCommonName(commonName);
-            updateCropDTO.setScientificName(scientificName);
-            updateCropDTO.setImage(base64ProfilePic);
-            updateCropDTO.setCategory(category);
-            updateCropDTO.setCropSeason(cropSeason);
-            updateCropDTO.setFieldCode(fieldCode);
-
-            cropService.updateCrop(cropCode,updateCropDTO);
+            String base64Image = AppUtil.toBase64Image(image);
+            CropDto cropDTO = new CropDto();
+            cropDTO.setCommonName(commonName);
+            cropDTO.setScientificName(scientificName);
+            cropDTO.setImage(base64Image);
+            cropDTO.setCategory(category);
+            cropDTO.setCropSeason(cropSeason);
+            cropDTO.setFieldCode(fieldCode);
+            cropDTO.setLogCode(logCode);
+            cropService.updateCrop(id,cropDTO);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (CropNotFoundException e){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
     }
-
-
     @DeleteMapping(value = "/{cropCode}")
     public ResponseEntity<Void> deleteCrop(@PathVariable("cropCode") String cropCode){
         try{
@@ -98,5 +104,10 @@ public class CropController {
     @GetMapping(value = "allcrops", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CropDto> getAllCrops(){
         return cropService.getAllCrops();
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CropResponse getCropById(@PathVariable("id") String id){
+        return cropService.getSelectedCrop(id);
     }
 }
--- src/main/java/lk/ijse/gdse68/CropMonitoringSystem/service/impl/CropServiceImpl.java
+++ src/main/java/lk/ijse/gdse68/CropMonitoringSystem/service/impl/CropServiceImpl.java
@@ -4,10 +4,15 @@ import jakarta.transaction.Transactional;
 import lk.ijse.gdse68.CropMonitoringSystem.customObj.CropErrorResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.customObj.CropResponse;
 import lk.ijse.gdse68.CropMonitoringSystem.dao.CropDao;
+import lk.ijse.gdse68.CropMonitoringSystem.dao.FieldDao;
+import lk.ijse.gdse68.CropMonitoringSystem.dao.MonitoringLogDao;
 import lk.ijse.gdse68.CropMonitoringSystem.dto.impl.CropDto;
 import lk.ijse.gdse68.CropMonitoringSystem.entity.CropEntity;
+import lk.ijse.gdse68.CropMonitoringSystem.entity.FieldEntity;
+import lk.ijse.gdse68.CropMonitoringSystem.entity.MonitoringLogEntity;
 import lk.ijse.gdse68.CropMonitoringSystem.exception.CropNotFoundException;
 import lk.ijse.gdse68.CropMonitoringSystem.exception.DataPersistFailedException;
+import lk.ijse.gdse68.CropMonitoringSystem.exception.FieldNotFoundException;
 import lk.ijse.gdse68.CropMonitoringSystem.service.CropService;
 import lk.ijse.gdse68.CropMonitoringSystem.util.AppUtil;
 import lk.ijse.gdse68.CropMonitoringSystem.util.Mapping;
@@ -24,10 +29,17 @@ public class CropServiceImpl implements CropService {
     private CropDao cropDao;
 
     @Autowired
+    private FieldDao fieldDao;
+
+    @Autowired
+    private MonitoringLogDao monitoringLogDao;
+
+    @Autowired
     private Mapping mapping;
 
     @Override
     public void saveCrop(CropDto cropDto) {
+        // cropDto.setCropCode(cropDto.getCropId());
         cropDto.setCropCode(AppUtil.createCropId());
         var cropEntity = mapping.convertToEntity(cropDto);
         var saveCrop= cropDao.save(cropEntity);
@@ -39,18 +51,34 @@ public class CropServiceImpl implements CropService {
 
     @Override
     public void updateCrop(String cropCode, CropDto cropDto) {
-        Optional<CropEntity> tmpcropEntity = cropDao.findById(cropCode);
-        if(!tmpcropEntity.isPresent()){
-            throw new CropNotFoundException("crop not found");
-        }else{
+        Optional<CropEntity> byId = cropDao.findById(cropCode);
+        if (!byId.isPresent()) {
+            throw new CropNotFoundException("Couldn't find the crop!");
+        } else {
+            CropEntity cropEntity = byId.get();
+            // Set other fields
+            cropEntity.setCommonName(cropDto.getCommonName());
+            cropEntity.setScientificName(cropDto.getScientificName());
+            cropEntity.setImage(cropDto.getImage());
+            cropEntity.setCategory(cropDto.getCategory());
+            cropEntity.setCropSeason(cropDto.getCropSeason());
 
-            tmpcropEntity.get().setCommonName(cropDto.getCommonName());
-            tmpcropEntity.get().setScientificName(cropDto.getScientificName());
-            tmpcropEntity.get().setImage(tmpcropEntity.get().getImage());
-            tmpcropEntity.get().setCropSeason(tmpcropEntity.get().getCropSeason());
-            tmpcropEntity.get().setField(tmpcropEntity.get().getField());
+            // Fetch the FieldEntity based on fieldCode
+            Optional<FieldEntity> fieldEntityOpt = fieldDao.findById(cropDto.getFieldCode());
+            if (!fieldEntityOpt.isPresent()) {
+                throw new FieldNotFoundException("Field not found for fieldCode: " + cropDto.getFieldCode());
+            }
+            cropEntity.setField(fieldEntityOpt.get());
+
+            if (cropDto.getLogCode() != null) {
+                // If you want to update multiple fields, you would need to use findAllById
+                List<MonitoringLogEntity> logEntities = monitoringLogDao.findAllById(cropDto.getLogCode());
+                cropEntity.setLogEntities(logEntities);
+            }
+
+            // Save the updated cropEntity back to the database
+            cropDao.save(cropEntity);
         }
-
 
     }
 
